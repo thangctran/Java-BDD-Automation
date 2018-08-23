a4f61c3ca5b137f11e4bdec6ee86cb3c942a8efb
cuc.doan <cuc.doan@hotschedules.com>
2018-08-23 17:20:03 +0700

    Test

---
+++ Selenium_POM/src/test/java/Main.java
@@ -0,0 +1,14 @@
+import helpers.FileHelper;
+
+import java.util.Map;
+
+public class Main {
+    public static void main(String[] args) throws InterruptedException {
+        Map<String,String> a = FileHelper.getTestDataCSV("login\\BE002.csv", "\t" , 1);
+        Map<String,String> b = FileHelper.getTestDataCSV("login\\BE002.csv", "\t", 2);
+        System.out.println(a.get("email"));
+        System.out.println(a.get("error_message"));
+        System.out.println(b.get("email"));
+        System.out.println(b.get("password"));
+    }
+}
